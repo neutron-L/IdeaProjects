ab50b8a96f79d969916afbfaa34f6f38e257a9b0
Alex-Lin-Fei <2055017970@qq.com>
2021-05-04 10:26:40 +0800

    node.md

--- Spring/Hello/src/main/java/org/example/Hello.java
+++ Spring/Hello/src/main/java/org/example/Hello.java
@@ -1,7 +1,35 @@
 package org.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Set;
+
 public class Hello {
     private String name;
+    private String[] books;
+    private int age;
+
+    public Hello(String name, int age) {
+
+    }
+
+    public String[] getBooks() {
+        return books;
+    }
+
+    public void setBooks(String[] books) {
+        this.books = books;
+    }
+
+    public Set<Double> getScores() {
+        return scores;
+    }
+
+    public void setScores(Set<Double> scores) {
+        this.scores = scores;
+    }
+
+    private Set<Double> scores;
 
 
     public void setName(String name) {
--- Spring/Hello/src/test/java/org/example/HelloTest.java
+++ Spring/Hello/src/test/java/org/example/HelloTest.java
@@ -3,11 +3,14 @@ package org.example;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.Arrays;
+
 public class HelloTest {
 
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         Hello hello = (Hello) context.getBean("b");
         hello.getName();
+        System.out.println(Arrays.toString(hello.getBooks()));
     }
 }
