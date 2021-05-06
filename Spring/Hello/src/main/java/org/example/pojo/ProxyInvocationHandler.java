7ec2ea6211dc67f04a56bc634f28f841941acdff
Alex-Lin-Fei <2055017970@qq.com>
2021-05-06 18:57:04 +0800

    something

--- Spring/Hello/src/main/java/org/example/pojo/ProxyInvocationHandler.java
+++ Spring/Hello/src/main/java/org/example/pojo/ProxyInvocationHandler.java
@@ -1,4 +1,25 @@
 package org.example.pojo;
 
-public class ProxyInvocationHandler {
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+//自动生成代理类
+public class ProxyInvocationHandler implements InvocationHandler {
+    private Object target; //被代理的接口
+
+    public void setTarget(Object target) {
+        this.target = target;
+    }
+
+//    生成得到代理类
+    public Object getProxy() {
+        return Proxy.newProxyInstance(this.getClass().getClassLoader(), target.getClass().getInterfaces(), this);
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        Object res = method.invoke(proxy, args);
+        return res;
+    }
 }
